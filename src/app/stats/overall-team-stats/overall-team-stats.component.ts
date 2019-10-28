7ea80215c3a6c550c7c4af6363c184d8a0032910
Jeff Kwok <jeff@skymatics.com>
2019-10-28 15:36:39 -0600

    have stats into league, conference, division

--- src/app/stats/overall-team-stats/overall-team-stats.component.ts
+++ src/app/stats/overall-team-stats/overall-team-stats.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -11,27 +11,44 @@ import { takeWhile } from 'rxjs/operators';
   templateUrl: './overall-team-stats.component.html',
   styleUrls: ['./overall-team-stats.component.css']
 })
-export class OverallTeamStatsComponent implements OnInit, OnDestroy {
+export class OverallTeamStatsComponent implements OnInit, OnDestroy, AfterViewInit {
 
   private _alive:boolean = true;
   isLoading: boolean = false;
 
   stats: any[];
+  westernStats: any[];
+  easternStats: any[];
+  northWesternStats: any[];
+  northEasternStats: any[];
+  southWesternStats: any[];
+  southEasternStats: any[];
+  league: any;
 
   short_team_name: string = '';
 
+  northwestTeams = [];
+  southwestTeams = [];
+  northeastTeams = [];
+  southeastTeams = [];
+
   teams: MatTableDataSource<any[]>;
+  westernTeams: MatTableDataSource<any[]>;
+  easternTeams: MatTableDataSource<any[]>;
+  northWesternTeams: MatTableDataSource<any[]>;
+  northEasternTeams: MatTableDataSource<any[]>;
+  southWesternTeams: MatTableDataSource<any[]>;
+  southEasternTeams: MatTableDataSource<any[]>;
   teamsColumnsToDisplay = [
-    'team_name', 'games_played', 'wins', 'loss', 'ties', 'points', 'goals_for', 'goals_for_game', 'goals_against', 'goals_against_game', 
+    'team_logo','team_name', 'games_played', 'wins', 'loss', 'ties', 'points', 'goals_for', 'goals_for_game', 'goals_against', 'goals_against_game', 
     'goals_diff', 'win_pct', 'pp_pct', 'pk_pct', 'sh_goals', 'penalty_minutes_game', 'shot_diff', 'div_record',
     'home_record', 'away_record', 'trail_record'
   ];
 
   page: number = 1;
-  pageSize: number = 10;
+  pageSize: number = 20;
   length: number = 0;
 
-  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: false}) sort: MatSort;
 
   constructor(
@@ -41,6 +58,11 @@ export class OverallTeamStatsComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.isLoading = true;
+    this.league = this._teamsService.league;
+    this.northwestTeams = this._teamsService.league.conference[0].division[0].teams;
+    this.southwestTeams = this._teamsService.league.conference[0].division[1].teams;
+    this.northeastTeams = this._teamsService.league.conference[1].division[0].teams;
+    this.southeastTeams = this._teamsService.league.conference[1].division[1].teams;
     if (this._route.snapshot.routeConfig.path === "stats/league") {
       this._teamsService.getLeagueTeamsStats().pipe(takeWhile(() => this._alive)).subscribe(resp => {
         console.log(resp);
@@ -49,7 +71,6 @@ export class OverallTeamStatsComponent implements OnInit, OnDestroy {
         this.length = this.stats.length;
         this.isLoading = false;
         setTimeout(() => {
-          this.teams.paginator = this.paginator;
           this.teams.sort = this.sort;
         }, 350);
       });
@@ -62,10 +83,67 @@ export class OverallTeamStatsComponent implements OnInit, OnDestroy {
         this.length = this.stats.length;
         this.isLoading = false;  
         setTimeout(() => {
-          this.teams.paginator = this.paginator;
           this.teams.sort = this.sort;
         }, 350);
       });
+    }
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      // this.typeSelect.value = "league"
+    }, 250);
+    
+  }
+
+  findLogo(shortName) {
+    let team = this._teamsService.getTeamInfo(shortName);
+    return { image: team.image, name: team.name }
+  }
+
+  getConferenceStats() {
+    let westTeams = this.northwestTeams.concat(this.southwestTeams);
+    let eastTeams = this.northeastTeams.concat(this.southeastTeams);
+    this.westernStats = this.stats.filter(team => westTeams.find(divTeam => divTeam.shortName === team.team_name));
+    this.easternStats = this.stats.filter(team => eastTeams.find(divTeam => divTeam.shortName === team.team_name));
+    this.westernTeams = new MatTableDataSource<any[]>(this.westernStats);
+    this.easternTeams = new MatTableDataSource<any[]>(this.easternStats);
+    setTimeout(() => {
+      this.length = this.westernStats.length;
+      this.westernTeams.sort = this.sort;
+      this.easternTeams.sort = this.sort;
+      this.pageSize = 10;
+    }, 500);
+  }
+
+  getDivisionStats() {
+    this.northWesternStats = this.stats.filter(team => this.northwestTeams.find(divTeam => divTeam.shortName === team.team_name));
+    this.southWesternStats = this.stats.filter(team => this.southwestTeams.find(divTeam => divTeam.shortName === team.team_name));
+    this.northEasternStats = this.stats.filter(team => this.northeastTeams.find(divTeam => divTeam.shortName === team.team_name));
+    this.southEasternStats = this.stats.filter(team => this.southeastTeams.find(divTeam => divTeam.shortName === team.team_name));
+    this.northWesternTeams = new MatTableDataSource<any[]>(this.northWesternStats);
+    this.southWesternTeams = new MatTableDataSource<any[]>(this.southWesternStats);
+    this.northEasternTeams = new MatTableDataSource<any[]>(this.northEasternStats);
+    this.southEasternTeams = new MatTableDataSource<any[]>(this.southEasternStats);
+    setTimeout(() => {
+      this.northWesternTeams.sort = this.sort;
+      this.southWesternTeams.sort = this.sort;
+      this.northEasternTeams.sort = this.sort;
+      this.southEasternTeams.sort = this.sort;
+      this.pageSize = 5;
+      this.length = this.northWesternStats.length;
+    }, 500);
+  }
+
+  onTabChange(event) {
+    console.log(event);
+    if (event.tab.textLabel === "League") {
+      this.pageSize = 5;
+      this.length = 20;
+    } else if (event.tab.textLabel === "Conference") {
+      this.getConferenceStats();
+    } else if (event.tab.textLabel === "Division") {
+      this.getDivisionStats();
     }
   }
 
