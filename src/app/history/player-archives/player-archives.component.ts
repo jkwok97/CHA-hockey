2134b7661c3c86c49d2d3ce05ba7d832bad841b9
Jeff Kwok <jeff@skymatics.com>
2019-11-06 14:46:18 -0700

    have all-time players leaders

--- src/app/history/player-archives/player-archives.component.ts
+++ src/app/history/player-archives/player-archives.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { TeamsService } from 'src/app/teams/teams.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-player-archives',
   templateUrl: './player-archives.component.html',
   styleUrls: ['./player-archives.component.css']
 })
-export class PlayerArchivesComponent implements OnInit {
+export class PlayerArchivesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private _alive:boolean = true;
+  isLoading: boolean = false;
+
+  page: number = 1;
+  pageSize: number = 20;
+  length: number = 0;
+
+  players: MatTableDataSource<any[]>;
+  playersColumnsToDisplay = [ 'playing_year', 'season_type',
+    'team_logo', 'player_name', 'position', 'games_played','goals', 'assists', 'points','plus_minus', 'penalty_minutes', 'sh_goals',
+    'gw_goals', 'gt_goals', 'shots', 'shooting_pct', 'minutes_per_game', 'fo_pct', 'pass_pct', 'corner_pct', 'hits', 'blocked_shots'
+  ];
+
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+
+  constructor(
+    private _teamsService: TeamsService
+  ) { }
 
   ngOnInit() {
+    this.isLoading = true;
+    this._teamsService.getPlayerStats().pipe(takeWhile(() => this._alive)).subscribe(resp => {
+      console.log(resp);
+      let stats = resp as [];
+      this.players = new MatTableDataSource<any[]>(stats);
+      this.pageSize = 25;
+      this.length = stats.length;
+      this.isLoading = false;
+      setTimeout(() => {
+        this.players.paginator = this.paginator;
+        this.players.sort = this.sort;
+      }, 350);
+    });
+  }
+
+  findLogo(shortName) {
+    if (shortName) {
+      let team = this._teamsService.getTeamInfo(shortName);
+      return { image: team.image, name: team.name }
+    } else {
+      return { image: "../../assets/team_logos/Free_Agent_logo_square.jpg", name: "Free Agent"}
+    }
+  }
+
+  applyFilter(filterValue: string) {
+    this.players.filter = filterValue.trim().toLowerCase();
+    if (this.players.paginator) {
+      this.players.paginator.firstPage();
+    }
+  }
+
+  ngOnDestroy() {
+    this._alive = false;
   }
 
 }
