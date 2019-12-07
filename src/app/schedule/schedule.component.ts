847f49e2669276e109b1d677fdb32affc74b11f3
Jeff Kwok <jkwok97@gmail.com>
2019-12-07 13:53:55 -0700

    fixing schedule component to read for mobile

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
   styleUrls: ['./schedule.component.css']
 })
-export class ScheduleComponent implements OnInit {
+export class ScheduleComponent implements OnInit, OnDestroy {
+
+  private _alive:boolean = true;
+  isMobile: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.checkMobile();
+    console.log(this.isMobile);
+  }
+
+  checkMobile() {
+    if ( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i) ) {
+          this.isMobile = true;
+        } else {
+          this.isMobile = false;
+        }
+  }
+
+  ngOnDestroy() {
+    this._alive = false;
   }
 
 }
