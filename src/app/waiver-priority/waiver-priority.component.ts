9d434dde6a64c5c045f2f680435872eecb41955f
Jeff Kwok <jkwok97@gmail.com>
2019-12-07 14:33:49 -0700

    fixing waiver wire component to read for mobile

--- src/app/waiver-priority/waiver-priority.component.ts
+++ src/app/waiver-priority/waiver-priority.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-waiver-priority',
   templateUrl: './waiver-priority.component.html',
   styleUrls: ['./waiver-priority.component.css']
 })
-export class WaiverPriorityComponent implements OnInit {
+export class WaiverPriorityComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private _alive:boolean = true;
+  isMobile: boolean = false;
+
+  constructor() {
+    this.checkMobile();
+   }
 
   ngOnInit() {
+  }
+
+  checkMobile() {
+    if ( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i) ) {
+          this.isMobile = true;
+        } else {
+          this.isMobile = false;
+        }
+  }
+
+  ngOnDestroy() {
+    this._alive = false;
   }
 
 }
