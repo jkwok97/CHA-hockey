d3b3ca5274478ea2fc9435215813356fe4ebd8a9
Jeff Kwok <jkwok97@gmail.com>
2019-12-09 15:06:52 -0700

    minor view adjustments

--- src/app/history/champions/champions.component.ts
+++ src/app/history/champions/champions.component.ts
@@ -11,24 +11,39 @@ import { MatTableDataSource } from '@angular/material/table';
 export class ChampionsComponent implements OnInit, OnDestroy {
 
   private _alive:boolean = true;
+  isMobile: boolean;
 
   currentChamp: any;
   champions = [];
   champs: MatTableDataSource<any[]>;
 
   champsColumns = ['year_won', 'team_logo', 'team_name', 'owner_name'];
+  mobileChampsColumns = ['year_won', 'team_logo', 'owner_name'];
 
   constructor(
     private _teamsService: TeamsService
   ) { }
 
   ngOnInit() {
+    this.checkMobile();
     this.currentChamp = this._teamsService.getTeamInfo("STA");
     this._teamsService.getChampions("champ").pipe(takeWhile(() => this._alive)).subscribe(resp => {
       // console.log(resp);
       this.champions = resp as [];
       this.champs = new MatTableDataSource<any[]>(this.champions);
     });
+  }
+
+  checkMobile() {
+    if ( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i) ) {
+          this.isMobile = true;
+        } else {
+          this.isMobile = false;
+        }
   }
 
   findLogo(shortName) {
