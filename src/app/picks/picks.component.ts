97d3d9851e6a7f5cbff985b1e3df609f878c63d4
Jeff Kwok <jkwok97@gmail.com>
2019-12-10 10:04:08 -0700

    added spinner to show loading for two components

--- src/app/picks/picks.component.ts
+++ src/app/picks/picks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-picks',
@@ -9,12 +10,22 @@ export class PicksComponent implements OnInit, OnDestroy {
 
   private _alive:boolean = true;
   isMobile: boolean = false;
+  isLoading: boolean = false;
 
-  constructor() { 
+  picksPage: any;
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) { 
     this.checkMobile();
   }
 
   ngOnInit() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.picksPage = this.sanitizer.bypassSecurityTrustResourceUrl("https://docs.google.com/spreadsheets/d/e/2PACX-1vQhNH4rS2kl3afY5kfy6IpXo4x3u-XJnuBh01R4bleYWovIvt-pk2JhtzxW-10kMamkd2LgpzmvuiMP/pubhtml?gid=370157936&single=true&widget=false&headers=false&chrome=false");
+      this.isLoading = false;
+    }, 500);
   }
 
   checkMobile() {
--- src/app/waiver-priority/waiver-priority.component.ts
+++ src/app/waiver-priority/waiver-priority.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-waiver-priority',
@@ -9,12 +10,22 @@ export class WaiverPriorityComponent implements OnInit, OnDestroy {
 
   private _alive:boolean = true;
   isMobile: boolean = false;
+  isLoading: boolean = false;
 
-  constructor() {
+  waiverPage: any;
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) {
     this.checkMobile();
    }
 
   ngOnInit() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.waiverPage = this.sanitizer.bypassSecurityTrustResourceUrl("https://docs.google.com/spreadsheets/d/e/2PACX-1vT2ElnsHkbexgaMCN5AVxLhcF3_8vCi56L4QW_umZ-cuPOn3_EC3fHEuJgf7C1Hz61-uVhhSoaQqx-F/pubhtml?widget=false&headers=false&chrome=false");
+      this.isLoading = false;
+    }, 500);
   }
 
   checkMobile() {
