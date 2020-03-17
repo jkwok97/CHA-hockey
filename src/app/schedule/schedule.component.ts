418cf0e1ab6e2c1c233b29f8fc90aa038c59cd1b
Jeff Kwok <jkwok97@gmail.com>
2020-03-17 14:18:24 -0600

    changed days for schedule

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -18,7 +18,7 @@ export class ScheduleComponent implements OnInit, OnDestroy {
   schedulePage: any;
   currentSeason: string;
   // range: string = 'a402:f821';
-  currentDay: number = 211;
+  currentDay: number = 216;
   scheduleType: string = "day";
 
   days = [];
