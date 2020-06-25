12f195aad84d914371cd044104a6bb7d8242c093
Jeff Kwok <jkwok97@gmail.com>
2020-06-25 13:20:35 -0600

    fixed imports

--- src/app/_services/awards.service.ts
+++ src/app/_services/awards.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Champion, Scorer } from '../_models/awards';
+import { OwnerAward, PlayerAward } from '../_models/awards';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
@@ -14,49 +14,49 @@ export class AwardsService {
     private _http: HttpClient
   ) { }
 
-  getChampions(): Observable<Champion[]> {
+  getChampions(): Observable<OwnerAward[]> {
 
     return this._http.get(`${environment.back_end_url}/v2/awards/champions`).pipe(
       map(result => result['result'])
     )
   }
 
-  getScorers(): Observable<Scorer[]> {
+  getScorers(): Observable<PlayerAward[]> {
 
     return this._http.get(`${environment.back_end_url}/v2/awards/scorers`).pipe(
       map(result => result['result'])
     )
   }
 
-  getRookies(): Observable<Scorer[]> {
+  getRookies(): Observable<PlayerAward[]> {
 
     return this._http.get(`${environment.back_end_url}/v2/awards/rookies`).pipe(
       map(result => result['result'])
     )
   }
 
-  getDefense(): Observable<Scorer[]> {
+  getDefense(): Observable<PlayerAward[]> {
 
     return this._http.get(`${environment.back_end_url}/v2/awards/defense`).pipe(
       map(result => result['result'])
     )
   }
 
-  getGoalies(): Observable<Scorer[]> {
+  getGoalies(): Observable<PlayerAward[]> {
 
     return this._http.get(`${environment.back_end_url}/v2/awards/goalies`).pipe(
       map(result => result['result'])
     )
   }
 
-  getGms(): Observable<Scorer[]> {
+  getGms(): Observable<OwnerAward[]> {
 
     return this._http.get(`${environment.back_end_url}/v2/awards/gm`).pipe(
       map(result => result['result'])
     )
   }
 
-  getSeason(): Observable<Scorer[]> {
+  getSeason(): Observable<OwnerAward[]> {
 
     return this._http.get(`${environment.back_end_url}/v2/awards/season`).pipe(
       map(result => result['result'])
