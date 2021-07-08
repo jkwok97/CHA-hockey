1cc93f1b057efd4f26df649c15fd306c7aefa275
Jeff Kwok <jkwok97@gmail.com>
2021-07-08 11:43:01 -0600

    update tree

--- src/app/games/playoff-matchup-final-card/playoff-matchup-final-card.component.ts
+++ src/app/games/playoff-matchup-final-card/playoff-matchup-final-card.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-playoff-matchup-final-card',
   templateUrl: './playoff-matchup-final-card.component.html',
   styleUrls: ['./playoff-matchup-final-card.component.css']
 })
-export class PlayoffMatchupFinalCardComponent implements OnInit {
+export class PlayoffMatchupFinalCardComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input() homeTeam;
+  @Input() visTeam;
+  @Input() winningTeam;
+  @Input() losingTeam;
+  @Input() seriesDone: boolean;
+  @Input() seriesScore: string;
+  @Input() seriesTied: boolean;
 
 }
