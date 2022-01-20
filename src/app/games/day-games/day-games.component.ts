3f8187afa4d57f3b86975edce185d442e855ec46
Jeff Kwok <jkwok97@gmail.com>
2022-01-20 14:33:31 -0700

    update day

--- src/app/games/day-games/day-games.component.ts
+++ src/app/games/day-games/day-games.component.ts
@@ -16,7 +16,7 @@ export class DayGamesComponent implements OnInit, OnDestroy {
 
   currentSeason: string;
   dayValue: string = "day";
-  currentDay: number = 71;
+  currentDay: number = 76;
 
   groupGames: any;
 
