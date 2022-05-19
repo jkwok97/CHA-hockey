62d9a186b6863db84da395216d002021848c3882
Jeff Kwok <jkwok97@gmail.com>
2022-05-19 15:29:29 -0600

    update to playoffs

--- src/app/_services/current-season.service.ts
+++ src/app/_services/current-season.service.ts
@@ -1,38 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CurrentSeasonService {
-
   isOffSeason: boolean = false;
 
   currentSeasonCap: number = 102.7;
   nextSeasonCap: number = 102.7;
 
-  draftSeason: string = '2022'
+  draftSeason: string = "2022";
 
   currentSeason: string = this.handleSeason(this.isOffSeason);
-  currentSeasonType: string = 'Regular';
-  nextSeason: string = '2022-23'
+  currentSeasonType: string = "Regular";
+  nextSeason: string = "2022-23";
 
-  minGames: number = this.handleMinGames(this.isOffSeason, this.currentSeasonType);
+  minGames: number = this.handleMinGames(
+    this.isOffSeason,
+    this.currentSeasonType
+  );
 
   seasonHasPlayoffs: boolean = this.handlePlayoffButton();
-  
+
   // need to account for roster somehow
-  constructor() { }
+  constructor() {}
 
   handleSeason(bool: boolean) {
-    return bool ? '2020-21' : '2021-22'
+    return bool ? "2020-21" : "2021-22";
   }
 
   handleMinGames(bool: boolean, type: string) {
-    return type === 'Regular' ? bool ? 10 : 5 : 1;
+    return type === "Regular" ? (bool ? 10 : 5) : 1;
   }
 
   handlePlayoffButton() {
-    return this.currentSeason === '2021-22' ? false : true;
+    return this.currentSeasonType === "Playoffs" ? true : false;
   }
-
 }
