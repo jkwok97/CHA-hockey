f2380efe41e0bd7f4f853416fd1346e94ee67130
Jeff Kwok <jkwok97@gmail.com>
2022-05-19 15:26:24 -0600

    update to playoffs

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,23 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-games',
-  templateUrl: './games.component.html',
-  styleUrls: ['./games.component.css']
+  selector: "app-games",
+  templateUrl: "./games.component.html",
+  styleUrls: ["./games.component.css"],
 })
 export class GamesComponent implements OnInit {
-
   activeLinkIndex = -1;
 
   routes = [
-    { name: 'By Day', url: 'day', current: true, disabled: false },
-    { name: 'All', url: 'all', current: true, disabled: false },
-    { name: 'Playoffs', url: 'playoffs', current: true, disabled: true },
+    { name: "By Day", url: "day", current: true, disabled: false },
+    { name: "All", url: "all", current: true, disabled: false },
+    { name: "Playoffs", url: "playoffs", current: true, disabled: false },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../_services/auth.service';
-import { User } from '../_models/user';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../_services/auth.service";
+import { User } from "../_models/user";
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  selector: "app-navigation",
+  templateUrl: "./navigation.component.html",
+  styleUrls: ["./navigation.component.css"],
 })
 export class NavigationComponent implements OnInit, OnDestroy {
-
   loggedIn: boolean = false;
-  private _alive:boolean = true;
+  private _alive: boolean = true;
 
   currentUser: User;
 
@@ -21,44 +20,40 @@ export class NavigationComponent implements OnInit, OnDestroy {
   activeLinkIndex = -1;
 
   routes = [
-    {name: 'Statistics', url: 'stats/teams/leaders', current: false},
-    {name: 'Teams', url: 'teams', current: false},
-    {name: 'Draft', url: 'draft/picks', current: false},
-    {name: 'Games', url: 'games/day', current: false},
-    {name: 'Salaries', url: 'salary', current: false},
-    {name: 'Trades', url: 'trades', current: false},
-    {name: 'Ratings', url: 'ratings', current: false},
-    {name: 'Waiver Priority', url: 'waiver-priority', current: false},
-    {name: 'Awards', url: 'awards/champions', current: false},
-    {name: 'Rules & Prizes', url: 'rules/equalization', current: false},
+    { name: "Statistics", url: "stats/teams/leaders", current: false },
+    { name: "Teams", url: "teams", current: false },
+    { name: "Draft", url: "draft/picks", current: false },
+    { name: "Games", url: "games/playoffs", current: false },
+    { name: "Salaries", url: "salary", current: false },
+    { name: "Trades", url: "trades", current: false },
+    { name: "Ratings", url: "ratings", current: false },
+    { name: "Waiver Priority", url: "waiver-priority", current: false },
+    { name: "Awards", url: "awards/champions", current: false },
+    { name: "Rules & Prizes", url: "rules/equalization", current: false },
   ];
 
-  constructor(
-    private _router: Router, 
-    private _authService: AuthService,
-  ) { 
-
-    if (this._authService.currentUserValue) { 
+  constructor(private _router: Router, private _authService: AuthService) {
+    if (this._authService.currentUserValue) {
       this.loggedIn = true;
-      this._router.navigate(['login']);
+      this._router.navigate(["login"]);
     } else {
-      this._router.navigate(['login']);
+      this._router.navigate(["login"]);
     }
 
-    this._authService.currentUser.subscribe(x => this.currentUser = x);
+    this._authService.currentUser.subscribe((x) => (this.currentUser = x));
 
     this._router.events.subscribe((res) => {
-      this.activeLinkIndex = this.routes.indexOf(this.routes.find(tab => tab.url === '.' + this._router.url));
+      this.activeLinkIndex = this.routes.indexOf(
+        this.routes.find((tab) => tab.url === "." + this._router.url)
+      );
     });
   }
 
   ngOnInit() {
-    this._router.navigate(['login']);
-    
+    this._router.navigate(["login"]);
   }
 
   ngOnDestroy() {
     this._alive = false;
   }
-
 }
