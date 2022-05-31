af0e12c20d96a03fdff96fd9114307962cd81831
Jeff Kwok <jkwok97@gmail.com>
2022-05-31 14:19:13 -0600

    test

--- src/app/games/playoff-tree-games/playoff-tree-games.component.ts
+++ src/app/games/playoff-tree-games/playoff-tree-games.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { first } from 'rxjs/operators';
-import { TeamStat } from 'src/app/_models/team';
-import { CurrentSeasonService } from 'src/app/_services/current-season.service';
-import { TeamStatsService } from 'src/app/_services/team-stats.service';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { first } from "rxjs/operators";
+import { TeamStat } from "src/app/_models/team";
+import { CurrentSeasonService } from "src/app/_services/current-season.service";
+import { TeamStatsService } from "src/app/_services/team-stats.service";
 
 @Component({
-  selector: 'app-playoff-tree-games',
-  templateUrl: './playoff-tree-games.component.html',
-  styleUrls: ['./playoff-tree-games.component.css']
+  selector: "app-playoff-tree-games",
+  templateUrl: "./playoff-tree-games.component.html",
+  styleUrls: ["./playoff-tree-games.component.css"],
 })
 export class PlayoffTreeGamesComponent implements OnInit {
-
   playoffTeams$: Observable<any[]>;
 
   westernTeams: TeamStat[];
@@ -20,27 +19,36 @@ export class PlayoffTreeGamesComponent implements OnInit {
   constructor(
     private teamStatsService: TeamStatsService,
     private currentSeasonService: CurrentSeasonService
-  ) { 
-    this.playoffTeams$ = this.teamStatsService
-    .getTeamStatsBySeasonByTypeByConference(this.currentSeasonService.currentSeason, 'Regular');
+  ) {
+    this.playoffTeams$ =
+      this.teamStatsService.getTeamStatsBySeasonByTypeByConference(
+        this.currentSeasonService.currentSeason,
+        "Regular"
+      );
   }
 
   ngOnInit() {
-    this.playoffTeams$
-      .pipe(
-        first()
-      )
-      .subscribe((teams) => {
-        this.westernTeams = this.setConferenceStandings(teams.find((conference) => conference['name'] === 'Western').teams.slice(0, 8));
-        this.easternTeams = this.setConferenceStandings(teams.find((conference) => conference['name'] === 'Eastern').teams.slice(0, 8));
-      })
+    this.playoffTeams$.pipe(first()).subscribe((teams) => {
+      this.westernTeams = this.setConferenceStandings(
+        teams
+          .find((conference) => conference["name"] === "Western")
+          .teams.slice(0, 8)
+      );
+      this.easternTeams = this.setConferenceStandings(
+        teams
+          .find((conference) => conference["name"] === "Eastern")
+          .teams.slice(0, 8)
+      );
+    });
   }
 
   setConferenceStandings(teams) {
     const tempLeaders = teams;
-    
-    teams.forEach(element => {
-      let tempTeam = tempLeaders.find(team => team.shortname === element.shortname);
+
+    teams.forEach((element) => {
+      let tempTeam = tempLeaders.find(
+        (team) => team.shortname === element.shortname
+      );
 
       element.points = tempTeam.points;
       element.wins = tempTeam.wins;
@@ -48,26 +56,33 @@ export class PlayoffTreeGamesComponent implements OnInit {
       element.goals_against = tempTeam.goals_against;
     });
 
-    return teams.sort((a,b) => {
-      if (b.points === a.points) {
-        if (b.wins === a.wins) {
-          if ((b.goals_for-b.goals_against) === (a.goals_for-a.goals_against)) {
-            return b.goals_for - a.goals_for;
+    return teams
+      .sort((a, b) => {
+        if (b.points === a.points) {
+          if (b.wins === a.wins) {
+            if (
+              b.goals_for - b.goals_against ===
+              a.goals_for - a.goals_against
+            ) {
+              return b.goals_for - a.goals_for;
+            } else {
+              return (
+                b.goals_for - b.goals_against - (a.goals_for - a.goals_against)
+              );
+            }
           } else {
-            return (b.goals_for-b.goals_against) - (a.goals_for-a.goals_against);
+            return b.wins - a.wins;
           }
         } else {
-          return b.wins - a.wins;
+          return b.points - a.points;
         }
-      } else {
-        return b.points - a.points;
-      }
-    }).map((team) => ({
-      conferenceStanding: teams.findIndex((t) => t.shortname === team.shortname) + 1,
-      teamlogo: team.teamlogo,
-      shortname: team.shortname,
-      teamcolor: team.teamcolor
-    }));
+      })
+      .map((team) => ({
+        conferenceStanding:
+          teams.findIndex((t) => t.shortname === team.shortname) + 1,
+        teamlogo: team.teamlogo,
+        shortname: team.shortname,
+        teamcolor: team.teamcolor,
+      }));
   }
-
 }
