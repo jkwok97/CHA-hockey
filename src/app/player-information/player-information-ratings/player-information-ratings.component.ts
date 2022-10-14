9f3d854dfdf0b5908950dae101669add9a74f83e
Jeff Kwok <jkwok97@gmail.com>
2022-10-14 07:54:46 -0600

    update player info ratings

--- src/app/player-information/player-information-ratings/player-information-ratings.component.ts
+++ src/app/player-information/player-information-ratings/player-information-ratings.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { CurrentSeasonService } from 'src/app/_services/current-season.service';
-import { PlayerService } from 'src/app/_services/player.service';
-import { takeWhile } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { CurrentSeasonService } from "src/app/_services/current-season.service";
+import { PlayerService } from "src/app/_services/player.service";
+import { takeWhile } from "rxjs/operators";
 
 @Component({
-  selector: 'app-player-information-ratings',
-  templateUrl: './player-information-ratings.component.html',
-  styleUrls: ['./player-information-ratings.component.css']
+  selector: "app-player-information-ratings",
+  templateUrl: "./player-information-ratings.component.html",
+  styleUrls: ["./player-information-ratings.component.css"],
 })
 export class PlayerInformationRatingsComponent implements OnInit, OnDestroy {
-
   private _alive: boolean = true;
   isLoading: boolean = false;
 
@@ -20,27 +19,44 @@ export class PlayerInformationRatingsComponent implements OnInit, OnDestroy {
   statRatings: any[];
   goalieStatRatings: any[];
 
-  playersRateColumns = [ 
-    'c_rate', 'l_rate', 'r_rate', 'ld_rate', 'rd_rate', 'skating', 'speed', 'passing',
-    'shooting', 'face_off', 'forecheck', 'assist_rating', 'clear_crease', 'shot_block', 'pk', 'physical',
-    'rock', 'intimidation', 'game_fatigue', 'shift_fatigue'
+  playersRateColumns = [
+    "c_rate",
+    "l_rate",
+    "r_rate",
+    "ld_rate",
+    "rd_rate",
+    "skating",
+    "speed",
+    "passing",
+    "shooting",
+    "face_off",
+    "forecheck",
+    "assist_rating",
+    "clear_crease",
+    "shot_block",
+    "pk",
+    "physical",
+    "rock",
+    "intimidation",
+    "game_fatigue",
+    "shift_fatigue",
   ];
 
-  goalieRateColumns = [
-    'passing', 'skating', 'speed'
-  ];
+  goalieRateColumns = ["passing", "skating", "speed"];
 
   constructor(
     private _route: ActivatedRoute,
     private _currentSeasonService: CurrentSeasonService,
     private _playerService: PlayerService
-  ) { 
+  ) {
     const playerId = this._route.snapshot.parent.params.id;
     this.playerType = this._route.snapshot.parent.params.type;
     this.season = this._currentSeasonService.currentSeason;
     // this.season = '2021-21';
 
-    this.playerType === 'player' ? this.getPlayerRatings(playerId, this.season) : this.getGoalieRatings(playerId, this.season);
+    this.playerType === "player"
+      ? this.getPlayerRatings(playerId, "2022-23")
+      : this.getGoalieRatings(playerId, "2022-23");
   }
 
   ngOnInit() {
@@ -48,29 +64,32 @@ export class PlayerInformationRatingsComponent implements OnInit, OnDestroy {
   }
 
   showRateColumns() {
-    return this.playerType === 'player' ? this.playersRateColumns : this.goalieRateColumns;
+    return this.playerType === "player"
+      ? this.playersRateColumns
+      : this.goalieRateColumns;
   }
 
   getPlayerRatings(id: number, season: string) {
-    this._playerService.getPlayerRatings(id, season).pipe(
-      takeWhile(() => this._alive)
-    ).subscribe((ratings) => {
-      this.statRatings = [ratings];
-      this.isLoading = false;
-    })
+    this._playerService
+      .getPlayerRatings(id, season)
+      .pipe(takeWhile(() => this._alive))
+      .subscribe((ratings) => {
+        this.statRatings = [ratings];
+        this.isLoading = false;
+      });
   }
 
   getGoalieRatings(id: number, season: string) {
-    this._playerService.getGoalieRatings(id, season).pipe(
-      takeWhile(() => this._alive)
-    ).subscribe((ratings) => {
-      this.statRatings = [ratings];
-      this.isLoading = false;
-    })
+    this._playerService
+      .getGoalieRatings(id, season)
+      .pipe(takeWhile(() => this._alive))
+      .subscribe((ratings) => {
+        this.statRatings = [ratings];
+        this.isLoading = false;
+      });
   }
 
   ngOnDestroy(): void {
     this._alive = false;
   }
-
 }
